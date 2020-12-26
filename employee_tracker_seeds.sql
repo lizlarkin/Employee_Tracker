0441042980fb2b43ad60b38d8408e918227a4691
Liz <lizlarkin@highlandtechnology.com>
2020-12-26 14:45:05 -0800

    updated seeds file to add table associations

--- employee_tracker_seeds.sql
+++ employee_tracker_seeds.sql
@@ -4,49 +4,49 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-	id INT NOT NULL AUTO_INCREMENT,
+	department_id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(75) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (department_id)
 );
 
 CREATE TABLE role (
-	id INT NOT NULL AUTO_INCREMENT,
+	role_id INT NOT NULL AUTO_INCREMENT,
 	title VARCHAR(75) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department (id)
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (department_id) REFERENCES department (department_id)
 );
 
 CREATE TABLE employee (
-	employeeID INT NOT NULL AUTO_INCREMENT,
+	employee_id INT NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_id INT,
     manager_id INT,
-    roleID INT DEFAULT '' NOT NULL,
-    PRIMARY KEY (employeeID),
-    FOREIGN KEY (roleID) REFERENCES role (roleID),
-    FOREIGN KEY (manager_id) REFERENCES employee (employeeID)
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (role_id) REFERENCES role (role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee (employee_id)
 );
 
 INSERT INTO department (name)
-VALUES ("Sales"), ("Management"), ("Warehouse"), ("HR");
+VALUES ("Sales"), ("Management"), ("Warehouse"), ("Administration");
 
-INSERT INTO role (title, salary) VALUES ("Regional Manager", 100000);
-INSERT INTO role (title, salary) VALUES ("Assistant to the Regional Manager", 90000);
-INSERT INTO role (title, salary) VALUES ("Sales", 80000);
-INSERT INTO role (title, salary) VALUES ("Secretary", 70000);
-INSERT INTO role (title, salary) VALUES  ("Forklift Operator", 60000);
-INSERT INTO role (title, salary) VALUES ("HR", 80000);
+INSERT INTO role (title, salary, department_id) VALUES ("Regional Manager", 100000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Assistant to the Regional Manager", 90000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Sales", 80000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Secretary", 70000, 4);
+INSERT INTO role (title, salary, department_id) VALUES  ("Forklift Operator", 60000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("HR", 80000, 4);
 
-INSERT INTO employee (first_name, last_name) VALUES ("Michael", "Scott");
-INSERT INTO employee (first_name, last_name) VALUES ("Dwight", "Schrute");
-INSERT INTO employee (first_name, last_name) VALUES ("Jim", "Halpert");
-INSERT INTO employee (first_name, last_name) VALUES ("Pam", "Beasley");
-INSERT INTO employee (first_name, last_name) VALUES ("Toby", "Flenderson");
-INSERT INTO employee (first_name, last_name) VALUES ("Andy", "Bernard");
-INSERT INTO employee (first_name, last_name) VALUES ("Stanley", "Hudson");
-INSERT INTO employee (first_name, last_name) VALUES ("Roy", "Anderson");
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Michael", "Scott", 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Dwight", "Schrute", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jim", "Halpert", 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Pam", "Beasley", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Toby", "Flenderson", 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Andy", "Bernard", 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Stanley", "Hudson", 3, 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Roy", "Anderson", 5);
 
 SELECT * FROM department;
 SELECT * FROM role;
